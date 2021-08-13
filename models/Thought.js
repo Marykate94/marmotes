664ab3780deccd9d2a848ceeba64cbc6ff7f6a5c
Marykate Smith <Mkayy@Marykates-MacBook-Pro.local>
2021-08-12 20:01:17 -0400

    schemas in models created

--- models/Thought.js
+++ models/Thought.js
@@ -31,18 +31,40 @@ const thoughtSchema = new Schema(
 
 const ReactionSchema = new Schema(
     {
-        reactionId: {
-            
-        }
+      reactionId: {
+        type: Schema.Types.ObjectId,
+        default: () => new Types.ObjectId(),
+      },
+      reactionBody: {
+        type: String,
+        required: true,
+        maxlength: 280,
+      },
+      username: {
+        type: String,
+        required: true,
+      },
+      createdAt: {
+        type: Date,
+        default: Date.now,
+        get: createdAtVal => dateFormat(createdAtVal)
+      },
+    },
+    {
+      toJSON: {
+        virtuals: true,
+        getters: true
+      },
+      id: false
     }
-)
-
+  );
+  
 ThoughtSchema.virtual('reactionCount').get(function() {
     return this.reactions.length;
 });
 
 
 
-const ThoughtSchema = model('Thought', ThoughtSchema);
+const Thought = model('Thought', ThoughtSchema);
 
 module.exports = Thought;
