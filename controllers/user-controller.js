0fede948a102e498a6f83b7435a413c162d930d3
Marykate Smith <Mkayy@Marykates-MacBook-Pro.local>
2021-08-12 20:22:35 -0400

    added user-controller get, add, update and delete requests

---
+++ controllers/user-controller.js
@@ -0,0 +1,63 @@
+const { User } = require("../models");
+
+const userController = {
+    // get all users
+    getAllUsers(req, res) {
+        User.find({})
+            .populate({
+                path: 'friends',
+                select: '-__v'
+            })
+            .select('-__v')
+            .sort({ _id: -1 })
+            .then(dbUserData => res.json(dbUserData))
+            .catch(err => {
+                console.log(err);
+                res.sendStatus(400);
+            });
+    },
+
+    // get a single user by id and populated thought and friend data
+    getUserById({ params }, res) {
+        User.findOne({ _id: params.id })
+            .populate({ path: "thoughts", select: "-_V" })
+            .populate({ path: "friends", select: "-_V" })
+            .select("-_V")
+            .then(dbUserData => res.json(dbUserData))
+            .catch(err => {
+                console.log(err);
+                res.sendStatus(400);
+            });
+    },
+
+    // post a new user 
+    createUser({ body }, res) {
+        Pizza.create(body)
+            .then(dbUserData => res.json(dbUserData))
+            .catch(err => res.json(err));
+    },
+
+    // put to update a user by its id
+    updateUser({ params, body }, res) {
+        User.findOneAndUpdate({ _id: params.id }, body, { new: true, runValidators: true })
+          .then(dbUserData => {
+            if (!dbUserData) {
+              res.status(404).json({ message: 'No user was found with this id!' });
+              return;
+            }
+            res.json(dbUserData);
+          })
+          .catch(err => res.json(err));
+      },
+
+    // delete to remove user by its id
+    deleteUser({ params }, res) {
+        User.findOneAndDelete({ _id: params.id })
+          .then(dbUserData => res.json(dbUserData))
+          .catch(err => res.json(err));
+      }
+
+    //bonus remove users associated thoughts when deleted 
+};
+
+module.exports = userController;
